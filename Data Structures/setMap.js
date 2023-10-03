f8a92a1f9f2ffe3cde802dce79c17ffe2b18b01a
Mikhail Melnikov <mihail3197@icloud.com>
2023-10-03 10:00:06 -0700

    set and map

---
+++ Data Structures/setMap.js
@@ -0,0 +1,14 @@
+const map = new Map();
+const objKey = { id: 5 };
+
+map.set(objKey, 'Maria');
+console.log(map.get(objKey));
+
+const set = new Set();
+
+set.add(5);
+set.add(5);
+set.add(3);
+set.add(6);
+
+console.log(set);
